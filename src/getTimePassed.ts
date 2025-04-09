201d311f07c0799f368b6de81e7d5ef961b66137
kd0010 <65524828+kd0010@users.noreply.github.com>
2025-04-09 15:12:46 +0300

    fix

--- src/getTimePassed.ts
+++ src/getTimePassed.ts
@@ -4,14 +4,15 @@ import {Clock} from './createClock'
  * Returns Clock's active time in milliseconds.
  */
 export function getActiveTime(clock: Clock): number {
+  const nowTime = Date.now()
   if (!clock.isPaused) {
-    return Date.now() - clock.beginTime - clock.totalPauseTime
+    return nowTime - clock.beginTime - clock.totalPauseTime
   } else {
     // `totalTimePaused` has not been updated yet
     // for the current pausing cycle,
     // therefore have to take that into account.
-    const currentTimePaused = Date.now() - clock.pausedAt
-    return Date.now()
+    const currentTimePaused = nowTime - clock.pausedAt
+    return nowTime
       - clock.beginTime
       - clock.totalPauseTime
       - currentTimePaused
